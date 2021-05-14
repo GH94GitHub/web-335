147af604b2a586353921242bdccc29fffce02f87
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-05-14 16:32:03 -0500

    Fixed return value to find_one

--- week-9/henderson_user-update.py
+++ week-9/henderson_user-update.py
@@ -25,5 +25,12 @@ db.users.update_one(
 )
 
 pprint.pprint(db.users.find_one(
-    {"employee_id":"01005"}
+    {"employee_id":"01005"},
+    {
+        "_id":0,
+        "email":1,
+        "employee_id":1,
+        "first_name":1,
+        "last_name":1
+    }
 ))
